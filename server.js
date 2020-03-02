7499d4bcc8ae2deb23632b6a0e7053fff621607a
Carlos Eduardo Bussola Mancini <cadumancini@gmail.com>
2020-03-02 16:45:34 -0300

    Criando primeira rota + Utilizando Nodemon

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const app = express();
+
+// Criando primeira rota:
+app.get('/', (req, res) => {
+  res.send('Hello World!!!');
+});
+// '/' -> rota a ser acessada. nesse caso, raiz do projeto
+// param 'req' -> itens na requisição feita pelo browser (parametros, corpo, autenticacao, ip, etc...)
+// param 'res' -> resposta a dar para a requisição
+
 app.listen(3001); // setando para o node 'ouvir' a porta 3001 do browser
