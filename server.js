3f81ec473e5f99b2b259399617a76af5812e758c
cadumancini <cadumancini@gmail.com>
2020-03-04 23:32:19 -0300

    Instalando MongoDB + Conectando Database

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// Iniciando app
 const app = express();
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
 // Criando primeira rota:
 app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+  res.send('Hello World!');
 });
 // '/' -> rota a ser acessada. nesse caso, raiz do projeto
 // param 'req' -> itens na requisição feita pelo browser (parametros, corpo, autenticacao, ip, etc...)
