ecdd020fdb28b35c6e389a7d50082bfe300cd186
cadumancini <cadumancini@gmail.com>
2020-03-06 20:51:33 -0300

    Reestruturação de arquivos

--- server.js
+++ server.js
@@ -12,20 +12,7 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true
 // Para não usar linha acima, usar o requireDir(<caminhoModels>)
 requireDir('./src/models');
 
-const Product = mongoose.model('Product'); // objeto do ORM para fazer insert, update, etc.
-
-// Criando primeira rota:
-app.get('/', (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  });
-
-  res.send('Hello World!');
-});
-// '/' -> rota a ser acessada. nesse caso, raiz do projeto
-// param 'req' -> itens na requisição feita pelo browser (parametros, corpo, autenticacao, ip, etc...)
-// param 'res' -> resposta a dar para a requisição
+// Rotas:
+app.use('/api', require('./src/routes'));
 
 app.listen(3001); // setando para o node 'ouvir' a porta 3001 do browser
