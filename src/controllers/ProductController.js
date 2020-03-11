fc48f9e3415b298b92b0c7b9fea9943e9f268495
Carlos Eduardo Bussola Mancini <cadumancini@gmail.com>
2020-03-11 16:50:15 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -8,8 +8,24 @@ module.exports = {
     return res.json(products); // API retornando em formato JSON
   },
 
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
+  },
+ 
   async store(req, res) {
     const product = await Product.create(req.body);
     return res.json(product);
+  },
+
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    // o new: true faz com que retorne o objeto com os novos valores
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+    return res.send(); // vai retornar uma reposta de SUCESSO sem conteúdo
   }
 };
